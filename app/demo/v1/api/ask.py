e35fcfcf9d7ef9019f4b3ec782b5bafed6599072
freon-lunarion <antonio.daniswara@gmail.com>
2019-03-17 20:42:23 +1100

    put response

--- app/demo/v1/api/ask.py
+++ app/demo/v1/api/ask.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, print_function
 
 from flask import request, g
+from rivescript import RiveScript
 
 from . import Resource
 from .. import schemas
@@ -11,5 +12,13 @@ class Ask(Resource):
 
     def post(self):
         print(g.json)
+        bot = RiveScript()
+        bot.load_directory("app/demo/v1/api/brain")
+        bot.sort_replies()
 
-        return None, 200, None
+        reply = bot.reply("user1", g.json['message'])
+
+
+        output = {"message": reply}
+
+        return output, 200, None
